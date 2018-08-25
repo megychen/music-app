6a34731b54af921dab1ffcf7969dd5e2d4135b66
megychen <megy.chen203@gmail.com>
2018-08-25 12:51:09 +0800

    Init project

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    Hello World
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style lang="styl" scoped>
+  @import '~common/stylus/variable'
+  #app
+    color: $color-theme
+</style>
